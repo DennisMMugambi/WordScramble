24c0af5b71f1165333fcd99108cb129df54c9ba3
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 12:00:43 +0300

    fetches data from file in bundle and starts game

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -23,16 +23,21 @@ struct ContentView: View {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 
                 Section {
-                    ForEach(usedWords, id: \.self) { word in
-                        Text(word)
-                    }
+                        ForEach(usedWords, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
+                        }
                 }
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
     }
     
@@ -45,8 +50,31 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
         
         //extra validation to come
-        usedWords.insert(answer, at: 0)
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
         newWord = ""
+    }
+    
+    func startGame() {
+        
+        //find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            //load contents of start.txt into string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                //split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                //pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkWorm"
+                
+                //if we are here everythiing has worked, so we can exit
+                return
+            }
+        }
+        
+        //if we are here then there was a problem -trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
